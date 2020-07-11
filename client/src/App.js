e4bf668f979412676d5f0792009026dedff48801
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2020-07-11 11:56:26 -0400

    implement eslint for node and react

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route, Link
-} from "react-router-dom";
-import API from './utils/API';
+	BrowserRouter as Router,
+	Switch,
+	Route, Link
+} from 'react-router-dom';
+import API from './utils/API.js';
 
-import './style.css'
+import './App.css';
 
-import { Login, Register } from './pages'
-import { Button } from './components/Input';
+import { Login, Register } from './pages.js';
+import { Button } from './components/Input.js';
 function App() {
-  const [user, setUser] = useState();
-  const getUser = async () => {
-    let user = await JSON.parse(localStorage.getItem('user'))
-    setUser(user)
-  }
-  useEffect(() => {
-    getUser();
-  }, [])
-  return (
-    <div className="main-container">
-      {user && <Button onClick={() => { API.logout(); setUser(); }}>Log Out {user.firstName}</Button>}
-      <Router>
-        {!user && <Link to="/login"><Button>Login</Button></Link>}
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/">
-            {/* <Home /> */}
-          </Route>
-        </Switch>
-      </Router>
-    </div >
-  );
+	const [user, setUser] = useState();
+	const getUser = async () => {
+		let user = await JSON.parse(localStorage.getItem('user'));
+		setUser(user);
+	};
+	useEffect(() => {
+		getUser();
+	}, []);
+	return (
+		<div className="main-container">
+			{user && <Button onClick={() => { API.logout(); setUser(); }}>Log Out {user.firstName}</Button>}
+			<Router>
+				{!user && <Link to="/login"><Button>Login</Button></Link>}
+				<Switch>
+					<Route path="/login">
+						<Login />
+					</Route>
+					<Route path="/register">
+						<Register />
+					</Route>
+					<Route path="/">
+						{/* <Home /> */}
+					</Route>
+				</Switch>
+			</Router>
+		</div >
+	);
 }
 
 export default App;
